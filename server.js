8af28aa118d0205e9146cd218d9167780fe965de
BobbyL2k <bobby_nh@hotmail.com>
2016-08-18 01:51:45 +0700

    Init gh-pages

--- server.js
+++
@@ -1,8 +0,0 @@
-/* jshint node: true, esversion: 6*/
-const express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-
-app.listen(3000);
-console.log("Server started on port 3000");
